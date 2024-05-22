1b6ea79aeef2967e84fbb86fbcaffc6c20698f1e
Matei Marica <matei.marica@diconium.com>
2024-05-22 16:32:23 +0300

    dockerfile and startup script for runner

---
+++ docker/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+FULL_PATH=$(dirname "$(realpath $0)")
+echo Trying to register runner on repo: $REPO with labels: $LABELS
+REG_TOKEN=$(curl -s -X POST -H "Authorization: token ${PAT}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output) 
+echo Using temporary registration token: $REG_TOKEN
+$FULL_PATH/config.sh --unattended --labels $RUNNER_LABELS --url https://github.com/${REPO} --token ${REG_TOKEN} 
+cleanup() 
+    { 
+        echo "Removing runner... " 
+        $FULL_PATH/config.sh remove --token ${REG_TOKEN}
+        exit 0
+    } 
+trap cleanup EXIT 
+trap cleanup TERM
+trap cleanup HUP
+trap cleanup INT
+$FULL_PATH/run.sh
