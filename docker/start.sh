51e4ffafc71204dd6197313d31f5032a033df726
Matei Marica <matei.marica@diconium.com>
2024-06-05 11:41:53 +0300

    added mechanism to support runner labels, added mechanism to remove the runner when container stops

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 FULL_PATH=$(dirname "$(realpath $0)")
-echo Trying to register runner on repo: $REPO with labels: $LABELS
+echo Trying to register runner on repo: $REPO with labels: $RUNNER_LABELS
 REG_TOKEN=$(curl -s -X POST -H "Authorization: token ${PAT}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output) 
 echo Using temporary registration token: $REG_TOKEN
 $FULL_PATH/config.sh --unattended --labels $RUNNER_LABELS --url https://github.com/${REPO} --token ${REG_TOKEN} 
@@ -10,8 +11,5 @@ cleanup()
         $FULL_PATH/config.sh remove --token ${REG_TOKEN}
         exit 0
     } 
-trap cleanup EXIT 
-trap cleanup TERM
-trap cleanup HUP
-trap cleanup INT
-$FULL_PATH/run.sh
+trap 'cleanup' SIGINT SIGTERM
+$FULL_PATH/run.sh & wait $!
